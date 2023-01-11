6dbd3ea500c9887de6009c2010ad5744ccdc4454
Nithish-maker <nithishraina0323@gmail.com>
2023-01-11 11:58:00 +0530

    dijkstras algo with sets

---
+++ graph/dijkstraUsingSet.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S) {
+        set<pair<int,int>>s;            // [dist, node]
+        vector<int>dist(V, 1e9);
+        dist[S] = 0;
+        s.insert({0, S});
+        while(!s.empty()) {
+            auto it = *(s.begin());
+            int dis = it.first;
+            int node = it.second;
+            s.erase(it);
+            for(auto it : adj[node]) {
+                int edgeWeight = it[1];
+                int adjNode = it[0];
+                if(dis + edgeWeight < dist[adjNode]) {
+                    if(dist[adjNode] != 1e9)
+                        s.erase({dist[adjNode], adjNode});
+                    dist[adjNode] = dis + edgeWeight;
+                    s.insert({dist[adjNode], adjNode});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i = 0;
+        while (i++ < E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1, t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin >> S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i = 0; i < V; i++)
+    	    cout << res[i] << " ";
+    	cout << endl;
+    }
+
+    return 0;
+}
